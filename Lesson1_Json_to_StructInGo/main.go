0eea333b9961502e95b330c392d90f03cc7f236f
Pedro Dias ☁️ <79172756+pedroodias7@users.noreply.github.com>
2024-09-22 15:49:08 +0100

    Update main.go

--- Lesson1_Json_to_StructInGo/main.go
+++ Lesson1_Json_to_StructInGo/main.go
@@ -1,19 +1,68 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io"
 	"os"
+	"fmt"
+	"log"
+	"net/url"
+	"net/http"
 )
 
+
+//json to Struct Golang
+type Words struct{
+	Page string `json:"page"`
+	Input string `json:"input"`
+	Words []string `json:"words"`
+}
+
 func main() {
+
 	args := os.Args
 
 	if len(args) < 2 {
-		fmt.Println("Usage: ./hello-world <argument>\n")
-		os.Exit()
+		fmt.Printf("Usage: ./http-get <url>\n")
+		os.Exit(1)
+	}
+
+	_, err := url.ParseRequestURI(args[1]) //if _,err := url.ParseRequestURI(ags[1]); err != nill
+	if err != nil {
+		fmt.Printf("URL is in invalid format: %v\n", err)
+	  	panic(err)	
+	}
+	
+	
+	res, err := http.Get(args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+
+	if res.StatusCode != 200 {
+		fmt.Printf("Invalid http code %d is in invalid format: %v\n", res.StatusCode, body)
+		os.Exit(1)
 	}
 
 
-	fmt.Printf("Hello World\nos.Args: %v\n 1º Arguments: %v", args, args[1])
+	var words Words
+
+
+	err = json.Unmarshal(body.&words)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("JSON PARSE\nPage: %s\nWords: %v\n", words.Page, strings.Join(words.Words,","))
+
+
 
 }
